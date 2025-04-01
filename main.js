b86845776bdf76488100e4a64145bc95a8588089
kalebeofc <kalebeofc@gmail.com>
2025-04-01 00:43:07 +0000

    criacao das ultimas logicas

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ let buttonSelected = null
 function receiveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
 
-    console.log(bill)
+    calculeteResults()
 }
 
 function receiveNumberofPeopleValue(){
     numberOfPeople = document.querySelector("#people").valueAsNumber
 
-    console.log(numberOfPeople)
+    calculeteResults()
 }
 
 function receiveTipPercentageValue(value){
@@ -26,13 +26,14 @@ function receiveTipPercentageValue(value){
 
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
+    calculeteResults()
 }
 
 function receiveCustomTipPercentageValue(){
     tipPercenta = document.querySelector("#custom-tip").valueAsNumber / 100
 
     removeClassButtonSelected()
-
+    calculeteResults()
 }
 
 function removeClassButtonSelected(){
@@ -40,4 +41,31 @@ function removeClassButtonSelected(){
         buttonSelected.classList.remove("button-selected")
         buttonSelected = null
     }
+}
+
+function calculeteResults(){
+    if(bill !== 0 && tipPercenta !== 0 && numberOfPeople !== 0){
+    let strongAmount = document.querySelector(".amount strong")
+    let tipAmountPerson = bill * tipPercenta / numberOfPeople
+    strongAmount.innerText = `$${tipAmountPerson.toFixed(2)}`
+
+    let strongTotal = document.querySelector(".total strong")
+    let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+    strongTotal.innerText = `$${totalPerson.toFixed(2)}`
+    }
+}
+
+function reset(){
+    document.querySelector("#bill").value = ""
+    let bill = 0
+    document.querySelector("#people").value = ""
+
+    document.querySelector("#custom-tip").value = ""
+    removeClassButtonSelected()
+    let tipPercenta = 0
+
+    document.querySelector(".amount strong").innerText = "$0.00"
+    document.querySelector(".total strong").innerText = "$0.00"
+
+
 }
