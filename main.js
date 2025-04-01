db988602c8fa723b600039cd6909149e1d7a3ed0
kalebeofc <kalebeofc@gmail.com>
2025-04-01 00:27:50 +0000

    criacao da logica de selecionar um botao da gorjeta ou colocar valor customizado

--- main.js
+++ main.js
@@ -18,10 +18,26 @@ function receiveNumberofPeopleValue(){
 function receiveTipPercentageValue(value){
     tipPercenta = value / 100
 
+        document.querySelector("#custom-tip").value = ""
+
     if (buttonSelected != null){
         buttonSelected.classList.remove("button-selected")
     }
 
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
+}
+
+function receiveCustomTipPercentageValue(){
+    tipPercenta = document.querySelector("#custom-tip").valueAsNumber / 100
+
+    removeClassButtonSelected()
+
+}
+
+function removeClassButtonSelected(){
+    if (buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
 }
